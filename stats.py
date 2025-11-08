2aebe5ee764dc19aa6cab3a574aae1a2c4962840
Andrew Taylor <andrewtaylorcode@gmail.com>
2025-11-08 17:32:34 -0600

    added new function to stats to get the number of characters

--- stats.py
+++ stats.py
@@ -4,3 +4,14 @@ def get_num_words(book_text):
     for word in list_of_words:
         number_of_words += 1
     return number_of_words
+
+def get_num_characters(book_text):
+    number_of_characters = {}
+    book_text = book_text.lower()
+    character_list = list(book_text)
+    for character in character_list:
+        if character in number_of_characters:
+            number_of_characters[character] += 1
+        else:
+            number_of_characters.update({character: 1})
+    return number_of_characters
